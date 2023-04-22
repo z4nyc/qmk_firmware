642b1b9cebbb9616add89e7cdf269e6d9aff6ef9
Esteban Osses-Anguita <esteban.osses@gmail.com>
2023-04-22 18:44:33 -0400

    Added new keymap

---
+++ keyboards/sofle/keymaps/eolatam_3/readme.md
@@ -0,0 +1,2 @@
+Personal keymap
+http://www.keyboard-layout-editor.com/#/gists/d54c8e973009a56dbbd73b36e869592f
---
+++ keyboards/sofle/keymaps/eolatam_3/rules.mk
@@ -0,0 +1,13 @@
+
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+ENCODER_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+
+CONSOLE_ENABLE = no
+LTO_ENABLE = yes
+
+LIB_SRC += local_init.c local_oled.c local_general.c
+
